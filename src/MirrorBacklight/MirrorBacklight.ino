4dab6425eb1006d762d13b01507575344338d3e7
piroxiljin <piroxiljin@gmail.com>
2025-06-22 22:18:05 +0700

    Add simple modulation based on PerlinNoise

--- src/MirrorBacklight/MirrorBacklight.ino
+++ src/MirrorBacklight/MirrorBacklight.ino
@@ -13,7 +13,9 @@
 #include <LittleFS.h>
 #include <WiFiConnector.h>
 
-#define VERSION "0.1"
+#include "ColorConverterLib.h"
+
+#define VERSION "0.1.1"
 
 GyverHub hub;
 PairsFile data(&LittleFS, "/data.dat", 3000);
@@ -47,11 +49,27 @@ bool bHueWaveEnable = true;
 bool bSolidColorEnable = true;
 gh::Flags fEnabledAnimations;
 
+struct SolidModulation {
+  bool Enabled;
+  float HueDepth;
+  uint16_t HueRate;
+  float SatDepth;
+  uint16_t SatRate;
+  float ValDepth;
+  uint16_t ValRate;
+};
+
+gh::Flag SendDebug;
+
 int solid_r = 128, solid_g = 0, solid_b = 0;
+SolidModulation solidModulationSettings;
+gh::Flag SolidModulationChanged;
 
 PGM_P BrightnessStr PROGMEM = "Brightness";
 PGM_P EnabledAnimationStr PROGMEM  = "EnabledAnimation";
 PGM_P SwitchIntervalStr PROGMEM  = "SwitchInterval";
+PGM_P DeviceNameStr PROGMEM  = "DeviceName";
+PGM_P SolidModulationStr PROGMEM  = "SolidModulation";
 
 gh::Flags FlagsFromString(const String& s) {
   gh::Flags result;
@@ -83,19 +101,21 @@ void onConnectClicked(gh::Build& b) {
 
 // билдер
 void build(gh::Builder& b) {
+  b.Menu("Calibrate");
+
   b.Title(F("Common"));
-  b.Slider_(BrightnessStr, &data).label((const __FlashStringHelper*)BrightnessStr).attach(onBrightnessChanged).range(0, 255, 1);
-  if (b.Flags_((const __FlashStringHelper*)EnabledAnimationStr, &fEnabledAnimations).label(F("Enabled animation")).text(F("Hue Wave;Solid Color")).click()) {
+  b.Slider_(BrightnessStr, &data).label(FPSTR(BrightnessStr)).attach(onBrightnessChanged).range(0, 255, 1);
+  if (b.Flags_(FPSTR(EnabledAnimationStr), &fEnabledAnimations).label(F("Enabled animation")).text(F("Hue Wave;Solid Color")).click()) {
     Serial.print(fEnabledAnimations.toString());
-    data[(const __FlashStringHelper*)EnabledAnimationStr] = fEnabledAnimations.toString();
+    data[FPSTR(EnabledAnimationStr)] = fEnabledAnimations.toString();
   };
 
-  b.Slider_((const __FlashStringHelper*)SwitchIntervalStr, &data).label(F("Switch interval")).range(1, 60, 0.5).suffix("sec");
+  b.Slider_(FPSTR(SwitchIntervalStr), &data).label(F("Switch interval")).range(1, 60, 0.5).suffix("sec");
 
   // b.Title("Hue Wave");
   // b.Switch(&bHueWaveEnable);
 
-  b.Title("Solid Color");
+  b.Title(F("Solid Color"));
   bool solidSliderClicked = (b.Slider_("SolidR", &data).range(0, 255, 1).label("R").click())
                             || (b.Slider_("SolidG", &data).range(0, 255, 1).label("G").click())
                             || (b.Slider_("SolidB", &data).range(0, 255, 1).label("B").click());
@@ -108,6 +128,20 @@ void build(gh::Builder& b) {
     hub.update("SolidColor").value(((solid_r & 0xff) << 16) | ((solid_g & 0xff) << 8) | (solid_b & 0xff));
   }
 
+  bool &modEnabled = solidModulationSettings.Enabled;
+  b.Switch(&solidModulationSettings.Enabled).label(F("Modulate")).attach(&SolidModulationChanged);
+  b.show(modEnabled);
+  b.Slider(&solidModulationSettings.HueDepth).label(F("Hue Depth")).range(0, 1.0, 0.001, 3).attach(&SolidModulationChanged);
+  b.Slider(&solidModulationSettings.HueRate).label(F("Hue Rate")).range(0, 1000, 10).attach(&SolidModulationChanged);
+  b.Slider(&solidModulationSettings.SatDepth).label(F("Saturation Depth")).range(0, 1.0, 0.001, 3).attach(&SolidModulationChanged);
+  b.Slider(&solidModulationSettings.SatRate).label(F("Saturation Rate")).range(0, 1000, 10).attach(&SolidModulationChanged);
+  b.Slider(&solidModulationSettings.ValDepth).label(F("Value Depth")).range(0, 1.0, 0.001, 3).attach(&SolidModulationChanged);
+  b.Slider(&solidModulationSettings.ValRate).label(F("Value Rate")).range(0, 1000, 10).attach(&SolidModulationChanged);
+  b.Button().label(F("Debug")).attach(&SendDebug);
+  b.show(true);
+
+  b.Title(F("Device"));
+  b.Input_(FPSTR(DeviceNameStr), &data).label(F("Device name"));
   b.Title("Network");
   b.Input_("ssid", &data);
   b.Pass_("pass", &data);
@@ -129,16 +163,16 @@ void setup() {
 #endif
   LittleFS.begin();
   data.begin();
-  if (!data.get((const __FlashStringHelper*)BrightnessStr).valid()) {
-    data[(const __FlashStringHelper*)BrightnessStr] = 128;
+  if (!data.get(FPSTR(BrightnessStr)).valid()) {
+    data[FPSTR(BrightnessStr)] = 128;
   }
 
-  if (!data.get((const __FlashStringHelper*)EnabledAnimationStr).valid()) {
+  if (!data.get(FPSTR(EnabledAnimationStr)).valid()) {
 
-    data[(const __FlashStringHelper*)EnabledAnimationStr] = gh::Flags(0xffff).toString();
+    data[FPSTR(EnabledAnimationStr)] = gh::Flags(0xffff).toString();
   }
-  if (!data.get((const __FlashStringHelper*)SwitchIntervalStr).valid()) {
-    data[(const __FlashStringHelper*)SwitchIntervalStr] = 5.0;
+  if (!data.get(FPSTR(SwitchIntervalStr)).valid()) {
+    data[FPSTR(SwitchIntervalStr)] = 5.0;
   }
   if (!data.get("SolidR").valid()) {
     data["SolidR"] = 255;
@@ -153,8 +187,17 @@ void setup() {
   solid_r = data.get("SolidR");
   solid_g = data.get("SolidG");
   solid_b = data.get("SolidB");
+  
+  if (!data[FPSTR(SolidModulationStr)].decodeB64(&solidModulationSettings, sizeof(solidModulationSettings))) {
+    memset(&solidModulationSettings, 0, sizeof(solidModulationSettings));
+    data[FPSTR(SolidModulationStr)] = pairs::Value(&solidModulationSettings, sizeof(solidModulationSettings));
+  }
 
-  hub.config(F("MyDevices"), F("Backlight"), "f2bd");  // user-circle icon
+  String deviceName = data.get(FPSTR(DeviceNameStr));
+
+  const __FlashStringHelper* backlight = F("Backlight");
+
+  hub.config(F("MyDevices"), deviceName.length() == 0 ? backlight : String(backlight) + "-" + deviceName, "f2bd");  // user-circle icon
   hub.setVersion(F("piroxiljin/mirror-backlight@" VERSION));
   hub.onBuild(build);
   hub.begin();
@@ -179,15 +222,39 @@ void setup() {
   WiFiConnector.connect(ssid, pass);
 
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is assumed
-  uint8_t bright = data.get((const __FlashStringHelper*)BrightnessStr);
+  uint8_t bright = data.get(FPSTR(BrightnessStr));
   FastLED.setBrightness(bright);
 
-  fEnabledAnimations = FlagsFromString(data.get((const __FlashStringHelper*)EnabledAnimationStr));
+  fEnabledAnimations = FlagsFromString(data.get(FPSTR(EnabledAnimationStr)));
 }
 
 void solidColorAnimation() {
+  uint32_t ms = millis();
+  
+	double hue, saturation, value;
+  RGBConverter::RgbToHsv(solid_r, solid_g, solid_b, hue, saturation, value);
+  hue *= 255;
+  saturation *= 255;
+  value *= 255;
+
+  if (solidModulationSettings.Enabled) {
+    hue += ((inoise16(ms * solidModulationSettings.HueRate, 0, 0) >> 8) - 128) * solidModulationSettings.HueDepth;
+    saturation += ((inoise16(ms * solidModulationSettings.SatRate, 1000, 1000) >> 8) - 128) * solidModulationSettings.SatDepth;
+    value += ((inoise16(ms * solidModulationSettings.ValRate, 2000, 2000) >> 8) - 128) * solidModulationSettings.ValDepth;
+  }
+
+  if (SendDebug) {
+    char buf[128];
+    sprintf_P(buf, PSTR("RGB: %d, %d, %d"), solid_r, solid_g, solid_b);
+    Serial.println(buf);
+    hub.sendCLI(buf);
+    sprintf_P(buf, PSTR("HSV: %f, %f, %f"), hue, saturation, value);
+    Serial.println(buf);
+    hub.sendCLI(buf);
+  }
+
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(solid_r, solid_g, solid_b);
+    leds[i] = CHSV(hue, saturation, value);
   }
 }
 
@@ -213,7 +280,11 @@ void loop() {
   WiFiConnector.tick();
   data.tick();
 
-  static int switchInterval = data.get((const __FlashStringHelper*)SwitchIntervalStr).toInt() * 1000;
+  if (SolidModulationChanged) {
+    data[FPSTR(SolidModulationStr)] = pairs::Value(&solidModulationSettings, sizeof(solidModulationSettings));
+  }
+
+  static int switchInterval = data.get(FPSTR(SwitchIntervalStr)).toInt() * 1000;
   static gh::Timer modeTimer(switchInterval);
 
   if (modeTimer) {
@@ -223,7 +294,7 @@ void loop() {
     }
     g_LightMode = ELightModes(nextMode);
 
-    switchInterval = data.get((const __FlashStringHelper*)SwitchIntervalStr).toInt() * 1000;
+    switchInterval = data.get(FPSTR(SwitchIntervalStr)).toInt() * 1000;
     modeTimer.startInterval(switchInterval);
   }
 
